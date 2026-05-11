af0a5e9e0f7602ea228ff4250a78cc22aeff7588
gyroradaza-meow <gyro.radaza12@gmail.com>
2026-05-11 11:05:53 +0800

    Category Controller

---
+++ src/main/resources/com/Category/CategoryController.java
@@ -0,0 +1,63 @@
+package com.Category;
+
+import Classes.Category.Category;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CategoryController {
+
+    @FXML
+    private TextField txtCategoryId;
+
+    @FXML
+    private TextField txtCategoryName;
+
+    @FXML
+    private TextArea txtDescription;
+
+    private Category category;
+
+    @FXML
+    public void handleSaveCategory() {
+
+        int id = Integer.parseInt(
+                txtCategoryId.getText());
+
+        String name =
+                txtCategoryName.getText();
+
+        String description =
+                txtDescription.getText();
+
+        category = new Category(
+                id,
+                name,
+                description);
+
+        System.out.println(
+                "Category Saved");
+    }
+
+    @FXML
+    public void handleEditCategory() {
+
+        if (category != null) {
+
+            category.editCategory(
+                    txtCategoryName.getText(),
+                    txtDescription.getText());
+
+            System.out.println(
+                    "Category Updated");
+        }
+    }
+
+    @FXML
+    public void handleClear() {
+
+        txtCategoryId.clear();
+        txtCategoryName.clear();
+        txtDescription.clear();
+    }
+}
