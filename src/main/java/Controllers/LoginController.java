4c14757d758b1146534ce6edea844dfa5f0a7417
kayzlyne <alidokheila7@gmail.com>
2026-05-12 17:07:54 +0800

    Add files via upload
    reuploading initial files due to errror

---
+++ src/main/java/Controllers/LoginController.java
@@ -0,0 +1,33 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void onLoginClick() throws IOException {
+        System.out.println("Login button clicked!");
+        System.out.println("Email: " + emailField.getText());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = (Stage) emailField.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Expense Tracker Dashboard");
+        stage.show();
+    }
+}
