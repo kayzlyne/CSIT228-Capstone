4dafaa8461d8049942c981be0ce98cde52f095e3
kayzlyne <alidokheila7@gmail.com>
2026-05-12 16:46:06 +0800

    Delete OOP2-Capstone/src/main/java/expense_tracker/oopcapstone directory

--- OOP2-Capstone/src/main/java/expense_tracker/oopcapstone/Launcher.java
+++
@@ -1,9 +0,0 @@
-package expense_tracker.oopcapstone;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(ExpenseTrackerApplication.class, args);
-    }
-}
