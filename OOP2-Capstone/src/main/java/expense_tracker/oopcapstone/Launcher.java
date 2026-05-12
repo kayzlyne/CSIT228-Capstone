677e99c9d55fa6a2d1166a8f898baaa58c537bf6
kayzlyne <alidokheila7@gmail.com>
2026-05-12 16:05:05 +0800

    Add files via upload
    Reuploading initial files due to errors

---
+++ OOP2-Capstone/src/main/java/expense_tracker/oopcapstone/Launcher.java
@@ -0,0 +1,9 @@
+package expense_tracker.oopcapstone;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ExpenseTrackerApplication.class, args);
+    }
+}
