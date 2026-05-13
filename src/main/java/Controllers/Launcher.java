fc88d2313f17a04c81fcf996e817a8c5adedec89
gyroradaza-meow <gyro.radaza12@gmail.com>
2026-05-13 12:43:08 +0800

    Delete src/main directory

--- src/main/java/Controllers/Launcher.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(ExpenseTrackerApplication.class, args);
-    }
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module Controllers {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens Controllers to javafx.fxml;
-    opens Users to javafx.fxml;
-    exports Controllers;
-    exports Users;
-}
